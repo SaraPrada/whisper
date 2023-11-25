f59d5bd4befe0e551009b01de7834f47d9748bf5
SaraPrada <prada_sara@libero.it>
2023-11-25 20:53:20 +0100

    click action on static messages

--- message.js
+++ message.js
@@ -2,10 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
   const sendButton = document.getElementById("send-button");
   const response = document.getElementById("response");
   const userInput = document.getElementById("message-input");
+  const staticResponse1 = document.getElementById("static-response1");
+  const staticResponse2 = document.getElementById("static-response2");
+
 
 
   sendButton.addEventListener("click", function () {
     response.style.display = "block";
   });
+
+  staticResponse1.addEventListener("click", function () {
+    if (staticResponse1.textContent === "ciao") {
+      staticResponse1.textContent = "siete usciti?";
+    } else {
+      staticResponse1.textContent = "ciao";
+    }
+  });
+
+  staticResponse2.addEventListener("click", function () {
+    if (staticResponse2.textContent === "ciao") {
+      staticResponse2.textContent = "voglio i gossip!";
+    } else {
+      staticResponse2.textContent = "ciao";
+    }
+  });
+
 
 });
